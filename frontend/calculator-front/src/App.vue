61eb0d206baf1d6d9a5f42a9fc494d7a2f6395e0
Cloudyee <gyonggyong98@gmail.com>
2023-08-11 14:13:14 +0900

    feat : 프론트 더하기 화면 기능 구현

--- frontend/calculator-front/src/App.vue
+++ frontend/calculator-front/src/App.vue
@@ -1,9 +1,18 @@
-<script setup>
-
-</script>
-
 <template>
   <h1>우린 최고야</h1>
+  <input type="number" v-mdodel.number="num1" placeholder="숫자를 입력하세요">
+  <button v-on:click="">+</button>
+  <input type="number" v-model.number="num2" placeholder="숫자를 입력하세요">
+  <div>{{ result }}</div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const num1 = ref(0);
+const num2 = ref(0);
+const result = ref(0);
+
+</script>
 
 <style scoped></style>
