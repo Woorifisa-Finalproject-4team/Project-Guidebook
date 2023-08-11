f1511815ab42fcf6242a07ace5b1c13206fa9155
leejunehee <junehee107@naver.com>
2023-08-11 15:58:46 +0900

    Feat : Add 테스트 코드 작성 완료

--- backend/src/main/java/com/example/backend/api/AddController.java
+++ backend/src/main/java/com/example/backend/api/AddController.java
@@ -4,25 +4,25 @@ import com.example.backend.model.Calculate;
 import com.example.backend.repository.CalcualteRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AddController {
     private final CalcualteRepository calcualteRepository;
 
+
     public AddController(CalcualteRepository calcualteRepository) {
         this.calcualteRepository = calcualteRepository;
     }
 
+
+
     @PostMapping("/toy/add")
     public ResponseEntity<Calculate> addVets(@RequestBody Calculate calculate){
-        int result = calculate.getNum1()+calculate.getNum2();
+       int result = calculate.getNum1()+calculate.getNum2();
         calculate.setResult(result);
-        System.out.println(result);
         calcualteRepository.save(calculate);
         System.out.println(new ResponseEntity<>(calculate, HttpStatus.CREATED));
         return new ResponseEntity<>(calculate, HttpStatus.CREATED);
